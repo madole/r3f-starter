833a3e872b2bf538813e1f92c5f9daab9ed81dbc
Andrew McDowell <madoliole@gmail.com>
2021-10-21 20:01:17 +1100

    removing unused styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 
 function App() {
 
-  return (
-    <div className="App">
-      <Canvas>
-        <OrbitControls />
-        <ambientLight />
-        <pointLight position={[0, 10, 2]} />
-        <mesh>
-          <boxGeometry args={[10, 5, 0.2]} />
-          <meshPhongMaterial color={"rebeccapurple"} attach={"material"} />
-        </mesh>
-      </Canvas>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Canvas>
+                <OrbitControls/>
+                <ambientLight/>
+                <pointLight position={[0, 10, 2]}/>
+                <mesh>
+                    <boxGeometry args={[10, 5, 0.2]}/>
+                    <meshPhongMaterial color={"rebeccapurple"} attach={"material"}/>
+                </mesh>
+            </Canvas>
+        </div>
+    )
 }
 
 export default App
